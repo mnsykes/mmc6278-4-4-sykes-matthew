9ba0728933fca6c372c073520a3117021866da1e
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2023-02-05 15:29:17 -0500

    complete

---
+++ queries/03-count-albums.sql
@@ -0,0 +1,4 @@
+-- Write a query to count the amount of albums in the albums table
+-- alias the count to "number_of_albums"
+SELECT COUNT(id) as number_of_albums
+FROM albums;
---
+++ schema/schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS music_db;
+
+USE music_db;
+
+CREATE TABLE artists (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100)
+);
+
+CREATE TABLE albums (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  artist_id INT NOT NULL,
+  release_year INT NOT NULL,
+  FOREIGN KEY (artist_id)
+    REFERENCES artists (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE songs (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  track_number INT NOT NULL,
+  artist_id INT NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY (artist_id)
+    REFERENCES artists (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (album_id)
+    REFERENCES albums (id)
+    ON DELETE CASCADE
+);
